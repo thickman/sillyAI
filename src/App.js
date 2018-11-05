d99d2568c93a4b59f6e9f2dc794e66ece1b0415f
Adam Olszewski <aolszewski00@gmail.com>
2018-11-05 10:44:22 +0100

    DEV: install and use sass instead css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import './styles/styles.css'
+import './App.scss';
+import './styles/styles.scss'
 
 class App extends Component {
   render() {
